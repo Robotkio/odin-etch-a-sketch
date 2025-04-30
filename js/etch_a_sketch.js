cea4969bc3af0b88adf9fec8e2c14ba62a0a93a7
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-04-29 23:30:00 -0700

    add grid resolution change button and function

--- js/etch_a_sketch.js
+++ js/etch_a_sketch.js
@@ -1,9 +1,22 @@
 let outputWindow = document.getElementById("eas-window");
+let setGridBtn = document.getElementById("set-grid-btn");
+
 let windowWidth = 900; //px
+const maxResolution = 100;
+const defaultGridSize = 16;
 
 outputWindow.style.width = `${windowWidth}px`;
+outputWindow.style.height = `${windowWidth}px`;
 
-function setupGrid(size = 16) {
+setGridBtn.addEventListener("click", () => {
+    let input = prompt("Input number of pixels for the grid:", defaultGridSize);
+    if (!isNaN(input) && input < maxResolution && input > 0) {
+        clearGrid();
+        setupGrid(input);
+    }
+})
+
+function setupGrid(size = defaultGridSize) {
     for (let i = 0; i < size*size; i++) {
         let square = document.createElement("div");
         square.classList.add("square");
@@ -11,10 +24,15 @@ function setupGrid(size = 16) {
         square.style.height = `${windowWidth / size}px`;
         square.addEventListener("mouseover", (e) => {
             e.target.style.backgroundColor = "black";
-            // console.log(e);
         });
         outputWindow.appendChild(square);
     }
 }
+
+function clearGrid() {
+    outputWindow.innerHTML = "";
+}
+
+
 
 setupGrid();
