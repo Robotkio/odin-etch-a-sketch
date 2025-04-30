b960d9129448dacac3db2fc2c904b132e08b1ea2
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-04-29 23:16:20 -0700

    add basic etch-a-sketch functionality

--- js/etch_a_sketch.js
+++ js/etch_a_sketch.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+let outputWindow = document.getElementById("eas-window");
+let windowWidth = 900; //px
+
+outputWindow.style.width = `${windowWidth}px`;
+
+function setupGrid(size = 16) {
+    for (let i = 0; i < size*size; i++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${windowWidth / size}px`;
+        square.style.height = `${windowWidth / size}px`;
+        square.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = "black";
+            // console.log(e);
+        });
+        outputWindow.appendChild(square);
+    }
+}
+
+setupGrid();
